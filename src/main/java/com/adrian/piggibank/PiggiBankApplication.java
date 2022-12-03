1bc02496ee858f666a72ad58bb7e7b4986c1a0c9
Adrian <adriankvn@gmail.com>
2022-12-02 23:07:33 -0500

    project start

---
+++ src/main/java/com/adrian/piggibank/PiggiBankApplication.java
@@ -0,0 +1,13 @@
+package com.adrian.piggibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiggiBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiggiBankApplication.class, args);
+    }
+
+}
